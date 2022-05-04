4f24c5a86e5fc63c26cc3112369171c48531304f
Tài Nguyễn Trọng <17521002@ms.uit.edu.vn>
2022-05-04 22:55:20 +0700

    Add Map

---
+++ 05-SceneManager/Map.cpp
@@ -0,0 +1,23 @@
+#include "Map.h"
+#include "Game.h"
+#include "debug.h"
+
+CMap::CMap(int TileSetID, int TotalRowsOfMap, int TotalColumnsOfMap, int TotalRowsOfTileSet, int TotalColumnsOfTileSet, int TotalTiles) {
+}
+
+CMap::~CMap()
+{
+}
+
+
+void CMap::DrawMap() {
+}
+
+void CMap::SetTileMapData(int** TileMapData) {
+}
+
+void CMap::ExtractTileFromTileSet() {
+}
+
+void CMap::SetCamPos(float x, float y) {
+}
---
+++ 05-SceneManager/Map.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <d3d10.h>
+#include"Textures.h"
+#include "Sprites.h"
+#include "Utils.h"
+
+#define TILE_WIDTH 16
+#define TILE_HEIGHT 16
+
+#define TILE_DIFF 1
+
+class CMap
+{
+	int TotalRowsOfMap, TotalColumnsOfMap;
+	int TotalRowsOfTileSet, TotalColumnsOfTileSet;
+	int TotalTiles;
+	LPTEXTURE TileSet; //sceneX_bank.png
+	vector<LPSPRITE> Tiles; // Danh sach Sprites cua TileSet
+	int** TileMap; // tmp 
+	float CamX, CamY;
+public:
+	CMap(int mapTextureID, int TotalRowsOfMap, int TotalColumnsOfMap, int TotalRowsOfTileSet, int TotalColumnsOfTileSet, int TotalTiles);
+	~CMap();
+	void ExtractTileFromTileSet();
+	void DrawMap();
+	void SetTileMapData(int** TileMapData);
+	int GetMapWidth() { return TotalColumnsOfMap * TILE_WIDTH; }
+	int GetMapHeight() { return TotalRowsOfMap * TILE_HEIGHT; }
+	void SetCamPos(float x, float y);
+};
